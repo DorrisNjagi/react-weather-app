0177885f35099c638104cb79063cb5147e5067a2
Dorris Nyawira Njagi <njagidoris@gmail.com>
2024-07-10 09:02:55 +0300

    added the GitHub URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather app
+     <h1>Weather app</h1>
+     <footer>
+      This project was coded by Dorris Njagi and {""}
+     <a href='https://github.com/DorrisNjagi/react-weather-app' target='blank'>
+     is open-sourced on GitHub
+     </a>
+     </footer>
     </div>
   );
 }
