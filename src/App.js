32cf7e864d2c783171c6361359056b566da54f3f
Dorris Nyawira Njagi <njagidoris@gmail.com>
2024-07-10 10:10:06 +0300

    checking if bootstrap is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from './weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
      <h1>Weather app</h1>
+    <Weather/>
      <footer>
       This project was coded by Dorris Njagi and {""}
      <a href='https://github.com/DorrisNjagi/react-weather-app' target='blank'>
