5d1dfd9119be1859b8185a7c5b8ee1dfd9f5e8f7
Dorris Nyawira Njagi <njagidoris@gmail.com>
2024-07-10 13:53:34 +0300

    added basic css styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1>Weather app</h1>
-    <Weather/>
+    <Weather />
      <footer>
-      This project was coded by Dorris Njagi and {""}
-     <a href='https://github.com/DorrisNjagi/react-weather-app' target='blank'>
-     is open-sourced on GitHub
+      This project was coded by Dorris Njagi  {""}
+     <a href='https://github.com/DorrisNjagi/react-weather-app' target='blank' >
+     and is open-sourced on GitHub
      </a>
      </footer>
     
--- src/weather.js
+++ src/weather.js
@@ -3,6 +3,44 @@ import "./weather.css";
 
 export default function Weather() {
     return(
-        <div>Hello weather app</div>
+        <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                <input type="search" placeholder="Enter a city..."  className="form-control" autoFocus="on"/>
+                </div>
+                <div className="col-3">
+                <input type="submit" value="search" className="btn btn-primary w-100"/>
+                
+                </div>
+                </div>
+            </form>
+            <h1>New York</h1>
+            <ul>
+                <li>Wednesday 07:00</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row" mt-3>
+                <div className="col-6">
+                    <div className="clearfix">
+                    < img src="https://up.yimg.com/ib/th?id=OIP.YKvFSocKFQaoo-56XlWNMwHaHa&pid=Api&rs=1&c=1&qlt=95&w=106&h=106" alt="mostly cold" className="float-left"/>
+                  <div className="float-letf">
+                      <span className="temperature">6</span>
+                      <span className="unit">°C</span>
+                  </div>
+                
+                </div>
+            </div>
+                <div className="col-6">
+                    <ul>
+                        <li>precipitation: 15%</li>
+                        <li>Humidity: 72%</li>
+                        <li>Wind: 13 km/h</li>
+
+                    </ul>
+                </div>
+
+            </div>
+        </div>
     );
 }
